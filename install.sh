55b77f0a57cf6f4d48724fae5a7f2017e6bbdd70
Chris Canal <dhtmlgod@gmail.com>
2021-03-29 13:47:33 -0600

    i3: Adds to install

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+set -e
+
+dest=~/.dotfiles
 id=~/.ssh/id_ed25519
+
 if [ ! -f "$id" ]; then
     echo "$id is not present. Exiting..."
     exit;
 fi
-##git clone --recursive git@github.com:carcer/dotphiles.git ~/.dotfiles-test
-echo $PWD
-#// Run deploy
-#// install nvm
-#// run dotsync
+
+git clone --recursive git@github.com:carcer/dotphiles.git $dest
+git checkout envs/i3
+cd $dest
+sh ./deploy/linux
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+$dest/dotsync/bin/dotsync -L
