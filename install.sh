afdfbc93b9ce24b67b243c218bee6100d5179ec9
Chris Canal <dhtmlgod@gmail.com>
2021-03-29 13:38:42 -0600

    i3: Testing path

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ if [ ! -f "$id" ]; then
     echo "$id is not present. Exiting..."
     exit;
 fi
-git clone --recursive git@github.com:carcer/dotphiles.git ~/.dotfiles-test
-
+##git clone --recursive git@github.com:carcer/dotphiles.git ~/.dotfiles-test
+echo $PWD
 #// Run deploy
 #// install nvm
 #// run dotsync
