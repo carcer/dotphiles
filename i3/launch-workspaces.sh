04a1c1ee5d0805adba99f8b1944cbb6fd0242a63
Chris Canal <chris@ocd.codes>
2026-03-02 14:47:06 +0000

    framework: stuff

---
+++ i3/launch-workspaces.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Launch all workspace applications
+
+i3-msg 'workspace 1; exec firefox'
+i3-msg 'workspace 2; exec code; exec kitty'
+i3-msg 'workspace 3; exec rider'
+i3-msg 'workspace 4; exec slack; exec discord'
+i3-msg 'workspace 5; exec postman'
+i3-msg 'workspace 8; exec spotify'
+i3-msg 'workspace 1'
